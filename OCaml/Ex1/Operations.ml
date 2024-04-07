78face184f9cc4a27e6197ba7f9403c8eb1a0aea
Sarp <Khiglix>
2024-04-07 22:50:30 +0300

    OCaml ex1

---
+++ OCaml/Ex1/Operations.ml
@@ -0,0 +1,18 @@
+let plus x y = x + y;;
+
+let minus x y = x - y;;
+
+let times x y = x * y;;
+
+let divide x y = x / y;;
+
+print_endline "Printing all Functions";;
+
+print_int (plus 10 5);;
+print_endline " <- (plus 10 5)";;
+print_int (minus 10 5);;
+print_endline " <- (minus 10 5)";;
+print_int (times 10 5);;
+print_endline " <- (times 10 5)";;
+print_int (divide 10 5);;
+print_endline " <- (divide 10 5)";;
