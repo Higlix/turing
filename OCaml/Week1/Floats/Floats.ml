ce9f98ab46e93fd7d7990240197803ea0983e064
Sarp <Khiglix>
2024-04-08 12:46:44 +0300

    Floats

---
+++ OCaml/Week1/Floats/Floats.ml
@@ -0,0 +1,7 @@
+3.0 +. 0.01;; (* -> (3.01) *)
+
+2e-4 *. 0.3;; (* -> (2e-05) *)
+
+2 + 3.0;; (* -> (TYPE ERROR) *) (* You can't add a int to a float *)
+
+4 *. 56;; (* -> (TYPE ERROR) *) (* You can't use float operation between ints *)
